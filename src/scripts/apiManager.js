b204a43b480796d938759e8e473a22b661e08b1b
Jisie DM <jisie.david@gmail.com>
2019-03-12 11:54:58 -0500

    Adding event listener for add flower button, get input values from form, create object for new flower and post to API

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -5,3 +5,14 @@ const getAllFlowers = () => {
   return fetch("http://localhost:8088/flowers")
   .then(response => response.json())
 }
+
+const postFlower = (newFlowerObject) => {
+  return fetch("http://localhost:8088/flowers", {
+    method: "POST",
+    body: JSON.stringify(newFlowerObject),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(response => response.json())
+}
--- src/scripts/eventHandlers.js
+++ src/scripts/eventHandlers.js
@@ -1,1 +1,33 @@
-console.log("eventHandlers.js")
+// console.log("eventHandlers.js")
+
+const handleAddFlower = () => {
+
+  const speciesInput = document.querySelector("#flower-species")
+  const quantityInput = document.querySelector("#flower-quantity")
+  const colorInput = document.querySelector("#flower-color")
+  const priceInput = document.querySelector("#flower-price")
+  const seasonalInput = document.querySelector("input[name='flower-seasonal']:checked")
+  const occasionInput = document.querySelector("#flower-occasion")
+
+  let newFlower = {
+    species: speciesInput.value,
+    quantity: Number(quantityInput.value),
+    color: colorInput.value,
+    price: Number(priceInput.value),
+    seasonal: (seasonalInput.value === "true"),
+    occasion: occasionInput.value
+  };
+
+  console.log("new flower", newFlower)
+
+  postFlower(newFlower)
+  .then(() => getAllFlowers())
+  .then(parsedResponse => appendAllFlowersToDom(parsedResponse))
+
+  speciesInput.value = ""
+  quantityInput.value = ""
+  colorInput.value = ""
+  priceInput.value = ""
+  occasionInput.value = ""
+  // document.querySelector("input[name='flower-seasonal']").checked = true
+};
